466865d748f61e720e7a98c96c1fe3808eff8df2
mexanist <a647503@gmail.com>
2025-03-01 23:56:53 +0700

    test

---
+++ Task1/main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+int main(){
+	sfd = socket_init();
+	if(sfd == 0){
+		perror("Error in func socket_init()");
+		exit(EXIT_FAILURE);
+	}
+/* Тест функции socket_init() */
+#if 1
+	if(test_of_socket_init(sfd) == -1){
+		perror("Test failed");
+		close(sfd);
+		exit(EXIT_FAILURE);
+	}
+#endif
+	close(sfd);
+}
+
